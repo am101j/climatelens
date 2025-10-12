60ebfca30058e278c98c49da942cd2039cd2e520
amakin <amakin@mc-2062-20.student.cs.uwaterloo.ca>
2025-10-11 23:51:30 -0400

    backend url fixed

--- climatelens-frontend/src/config.ts
+++ climatelens-frontend/src/config.ts
@@ -1,2 +1,1 @@
-//export const BACKEND_URL = "https://climatelens.onrender.com";
-export const BACKEND_URL = "http://127.0.0.1:8000"; // Backend runs on port 8000
+export const BACKEND_URL = "https://climatelens.onrender.com";
